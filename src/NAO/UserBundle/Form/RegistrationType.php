23a123ac49ccd89ded23edd5bc19ae1661d67ce9
Nam <supernam81@gmail.com>
2018-07-18 17:12:04 +0200

    1ère mise en place du backoffice. Création de 2 comptes utilisateurs test USER et NATURALISTE. Accès refusé au compte USER.

---
+++ src/NAO/UserBundle/Form/RegistrationType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace NAO\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+
+class RegistrationType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('lastname', TextType::class)
+        ->add('firstname', TextType::class)
+        ->add('town', TextType::class)
+        ->add('presentation', TextareaType::class)
+        ->add('newsletter', CheckboxType::class, array('required' => false))
+        ;
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'nao_user_registration';
+    }
+
+
+}
