52b6d5417f96abe75871e067f8a932b2f9df3cbd
Etheram68 <francois681927@hotmail.fr>
2018-07-23 22:08:26 +0200

    Modification template, Menu, CSS et Javascript, Ajout de la carte

--- src/NAO/CoreBundle/Controller/CoreController.php
+++ src/NAO/CoreBundle/Controller/CoreController.php
@@ -3,9 +3,26 @@
 namespace NAO\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 
+/**
+ * Class AppBundleController
+ *
+ * @package NAO\CoreBundle\Controller
+ */
 class CoreController extends Controller
 {
+	/**
+     * HomePage
+     * @Route("/", name="homepage")
+     * @Method({"GET"})
+     *
+     * @param Request $request Http request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function indexAction()
     {
         return $this->render('homepage\index.html.twig');
--- src/NAO/CoreBundle/Resources/public/js/main.js
+++ src/NAO/CoreBundle/Resources/public/js/main.js
@@ -12,21 +12,13 @@
     new WOW().init({
         mobile: true,
     });
-    /* Scroll to top
-    ===================*/
-    $.scrollUp({
-        scrollText: '<i class="fa fa-angle-up"></i>',
-        easingType: 'linear',
-        scrollSpeed: 900,
-        animation: 'fade'
-    });
     // :: 4.0 ScrollUp Active Code
-    // if ($.fn.scrollUp) {
-    //     $.scrollUp({
-    //         scrollSpeed: 1500,
-    //         scrollText: '<i class="fa fa-angle-up"></i>'
-    //     });
-    // }
+        if ($.fn.scrollUp) {
+            $.scrollUp({
+                scrollSpeed: 1500,
+                scrollText: '<i class="fa fa-angle-up"></i>'
+            });
+        }
     /* testimonials Slider Active
     =============================*/
     $('.testimonials').owlCarousel({
@@ -123,39 +115,7 @@
 
 
 
-    //Function to animate slider captions 
-    function doAnimations(elems) {
-        //Cache the animationend event in a variable
-        var animEndEv = 'webkitAnimationEnd animationend';
 
-        elems.each(function () {
-            var $this = $(this),
-                $animationType = $this.data('animation');
-            $this.addClass($animationType).one(animEndEv, function () {
-                $this.removeClass($animationType);
-            });
-        });
-    }
-
-    //Variables on page load 
-    var $myCarousel = $('.caption-slider'),
-        $firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
-
-    //Initialize carousel 
-    $myCarousel.carousel();
-
-    //Animate captions in first slide on page load 
-    doAnimations($firstAnimatingElems);
-
-    //Pause carousel  
-    $myCarousel.carousel('pause');
-
-
-    //Other slides to be animated on carousel slide event 
-    $myCarousel.on('slide.bs.carousel', function (e) {
-        var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
-        doAnimations($animatingElems);
-    });
 
 
 
