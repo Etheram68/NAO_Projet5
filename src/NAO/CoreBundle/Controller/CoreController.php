efc6b3c74343cf0fe871c5fd926cb173c2ff013d
araklesage <estebangrabette@yahoo.fr>
2018-08-01 21:47:38 +0200

    contact
    adding contact form and mail sending in coreController
    and article view in homepage

--- src/NAO/CoreBundle/Controller/CoreController.php
+++ src/NAO/CoreBundle/Controller/CoreController.php
@@ -2,6 +2,8 @@
 
 namespace NAO\CoreBundle\Controller;
 
+use NAO\CoreBundle\Entity\Contact;
+use NAO\CoreBundle\Form\ContactType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -14,7 +16,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
  */
 class CoreController extends Controller
 {
-	/**
+    /**
      * HomePage
      * @Route("/", name="homepage")
      * @Method({"GET"})
@@ -25,6 +27,57 @@ class CoreController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('homepage\index.html.twig');
+
+        $em = $this->getDoctrine()->getManager();
+
+        $listArticles = $em->getRepository('NAOBlogBundle:Article')->findBy(
+            array(),
+            array('date' =>'desc'),
+            3,
+            0
+        );
+        return $this->render('homepage\index.html.twig', array(
+            'listArticles' => $listArticles
+        ));
     }
+
+    /** ContactPage
+     * @Route("/contact", name="contact")
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function contactAction(Request $request)
+    {
+        $contact = new Contact();
+        $form = $this->get('form.factory')->create(ContactType::class, $contact);
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+
+            $mail = $form['mailAdress']->getData();
+            $name = $form['firstName']->getData();
+            $lastName = $form['lastName']->getData();
+            $town = $form['town']->getData();
+            $content = $form['content']->getData();
+
+
+            $message = (new \Swift_Message())
+                ->setFrom([$mail])
+                ->setTo('estebangrabette@gmail.com')
+                ->setBody($this->renderView('emails/contactMail.html.twig', array(
+                    'firstName' => $name,
+                    'lastName' => $lastName,
+                    'mailAdress' => $mail,
+                    'town' => $town,
+                    'content' => $content,
+                )));
+            $this->get('mailer')->send($message);
+
+            $request->getSession()->getFlashBag()->add('notice', 'Votre message à bien été envoyé');
+
+            $this->redirectToRoute('homepage');
+        }
+        return $this->render('core/contact.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
 }
