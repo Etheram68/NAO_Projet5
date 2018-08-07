fb1b5eed2dadbae149b21efbb1100b2b9e6107d4
Etheram68 <francois681927@hotmail.fr>
2018-08-07 12:36:14 +0200

    Ajout du controlleur et du dossier vue

--- src/NAO/FicheBundle/Controller/FicheController.php
+++ src/NAO/FicheBundle/Controller/FicheController.php
@@ -8,6 +8,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use NAO\FicheBundle\Entity\Bird;
+use NAO\FicheBundle\Form\Type\FicheType;
+
 
 /**
  * Class FicheController
@@ -16,5 +19,58 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
  */
 class FicheController extends Controller
 {
+    /**
+     * @Route("/", name="fiche.list")
+     * @Method ({"GET"})
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function showFicheAction(Request $request)
+    {
+        return $this->render('fiche/fiche.html.twig');
+    }
 
+    /**
+     * @Route("/creation", name="fiche.create")
+     * @Security("is_granted('ROLE_USER')")
+     * @Method({"GET", "POST"})
+     */
+    public function createAction(Request $request)
+    {
+        $bird = new Bird();
+        $form = $this->createForm(FicheType::class, $bird);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $action = $this->container->get('app.obs')->saveObservation($bird, $form, $request);
+            if($action == 'DRAFT'){
+                return $this->redirectToRoute('fiche.me.draft');
+            }elseif($action == 'WAITING'){
+                return $this->redirectToRoute('fiche.me.waiting');
+            }elseif($action == 'PUBLISHED'){
+                return $this->redirectToRoute('fiche.me.validate');
+            }
+        }
+        return $this->render('fiche/create.html.twig', array(
+            'form'      => $form->createView()
+        ));
+    }
+
+    /**
+     * @Route("/{id}", name="fiche.detail", requirements={"id": "\d+"})
+     * @ParamConverter("fiche", options={"mapping": {"id": "id"}})
+     * @Method({"GET"})
+     */
+    public function showDetailAction(Fiche $fiche)
+    {
+        if($fiche->getStatus() != $fiche::VALIDATED){
+            throw $this->createNotFoundException('Observation need to be validate !');
+        }
+        $em = $this->getDoctrine()->getManager();
+        $lastfiche = $em->getRepository('NAOFicheBundle:Bird')
+            ->getLastFicheForBird($fiche->getTaxref()->getId(),$fiche->getId());
+        return $this->render('fiche/detail.html.twig', array(
+            'fiche'   => $fiche,
+            'lastfiche'       => $lastfiche != [] ? $lastfiche[0] : null
+        ));
+    }
 }
