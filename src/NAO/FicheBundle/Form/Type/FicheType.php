24b49220e09e3adaee9e614f566091e38b5992cb
Etheram68 <francois681927@hotmail.fr>
2018-08-07 14:07:17 +0200

    Mise en place page création

--- src/NAO/FicheBundle/Form/Type/FicheType.php
+++ src/NAO/FicheBundle/Form/Type/FicheType.php
@@ -6,9 +6,12 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Validator\Constraints\File;
 
 class FicheType extends AbstractType
@@ -29,9 +32,10 @@ class FicheType extends AbstractType
                 'label'          => 'Principal Couleur'
             ))
             ->add('feature', TextareaType::class, array(
-                'requirep'       => false,
+                'required'       => false,
                 'label'          => 'Information complémentaire'
             ))
+            ->add('taxref', TaxrefType::class)
             ->add('imagepath', FileType::class, array(
                 'label'         => false,
                 'mapped'        => false,
