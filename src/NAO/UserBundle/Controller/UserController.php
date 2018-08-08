423e03058bf85cb86bed4b63bcc458bb0b22d685
Nam <supernam81@gmail.com>
2018-08-08 23:00:44 +0200

    pages de Profil : récup des dernières observations et commentaires de l'utilisateur.

--- src/NAO/UserBundle/Controller/UserController.php
+++ src/NAO/UserBundle/Controller/UserController.php
@@ -39,12 +39,38 @@ class UserController extends Controller
         if ( $user == null) {
             throw new NotFoundHttpException("Pseudo non valide");
         }
+        // Calcul du niveau par rapport aux points
         $points = $user->getPoints();
         $userLevel = $this->container->get('naouser.level.levelCalcul')->guessLevel($points);
-
+        // Récupération des observations
+        $listObservations = $user->getObservations();
+        $infosObs = [];
+        if (isset($listObservations) && !is_null($listObservations)) {
+            foreach ($listObservations as $observation) {
+                $infosObs[] = [
+                    'oiseau' => $observation->getTaxref()->getCommonName(),
+                    'lieu' => $observation->getPlace(),
+                    'date' => $observation->getWatched()
+                ];
+            }
+        }
+        // Récupération des commentaires
+        $listComments = $user->getComments();
+        $infosComment = [];
+        if (isset($listComments) && !is_null($listComments)) {
+            foreach ($listComments as $commentaire) {
+                $infosComment[] = [
+                    'date' => $commentaire->getDate(),
+                    'contenu' => $commentaire->getContent()
+                ];
+            }
+        }
         return $this->container->get('templating')->renderResponse('@FOSUser/Profile/show.html.twig', array(
             'user' => $user,
             'userLevel' => $userLevel,
+            'listObservations' => $listObservations,
+            'infosObs' => $infosObs,
+            'infosComment' => $infosComment,
         ));
     }
 
